dca8bfc93e4be9d682d6849d3791a2acb52f12c7
maxime surmont <maximesurmont@mbp-de-maxime.home>
2017-08-07 01:11:15 +0200

    content added, css print and small window for the next time

--- cv/cv.component.js
+++ cv/cv.component.js
@@ -10,6 +10,22 @@ angular.
         Informations.getInfo().$promise.then(function(res) {
             self.info = res;
             self.sectionDisplayed = 'proExp';
+
+            $(window).resize(function() {
+              var window_width = $(window).width();
+              $("#sectionContent").css('width', window_width-270+'px');
+            });
+            var window_width = $(window).width();
+            $("#sectionContent").css('width', window_width-270+'px');
+
+            self.sectionDisplay = function(section){
+              self.sectionDisplayed = section;
+            };
+
+            self.print = function(){
+              self.sectionDisplayed = "print";
+              window.print();
+            }
         });
       }
     ]
