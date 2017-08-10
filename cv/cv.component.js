f4a56e36c76f15c2d7a8c0b03ee1e2cdd827d214
maxime surmont <maximesurmont@mbp-de-maxime.home>
2017-08-10 16:48:58 +0200

    print and mobile version

--- cv/cv.component.js
+++ cv/cv.component.js
@@ -4,19 +4,42 @@ angular.
   module('cv').
   component('cv', {
     templateUrl: 'cv/cv.template.html',
-    controller: ['$routeParams', 'Informations',
-      function cvController($routeParams, Informations) {
+    controller: ['$routeParams', 'Informations', '$timeout',
+      function cvController($routeParams, Informations, $timeout) {
         var self = this;
         Informations.getInfo().$promise.then(function(res) {
             self.info = res;
             self.sectionDisplayed = 'proExp';
 
-            $(window).resize(function() {
+            var viewSection = function(){
               var window_width = $(window).width();
-              $("#sectionContent").css('width', window_width-270+'px');
+              if(window_width<=986){
+                $("body").css('width', window_width+'px');
+                $("#sectionContent").css('width', window_width-500+'px');
+                $("#divContent").css('width', window_width-50+'px');
+                $(".divSectionDisplay").css('width', window_width-50+'px');
+                $("#divSelectionPersExp").css('display', 'none');
+                $("#divSelectionFormation").css('display', 'none');
+                $("#divSelectionHobbies").css('display', 'none');
+                $("#divIcons").css('display', 'none');
+                $("#divImg img").css('left', window_width/2-80+'px');
+                self.sectionDisplayed = "print";
+              }else{
+                $("#sectionContent").css('width', window_width-400+'px');
+                $("#divContent").css('width', window_width-400+'px');
+                $(".divSectionDisplay").css('width', window_width-400+'px');
+                $("#divSelectionPersExp").css('display', '');
+                $("#divSelectionFormation").css('display', '');
+                $("#divSelectionHobbies").css('display', '');
+                $("#divImg img").css('left', '55px');
+                $("#divIcons").css('display', '');
+                self.sectionDisplayed = 'proExp';
+              }
+            }
+            $(window).resize(function() {
+              viewSection();
             });
-            var window_width = $(window).width();
-            $("#sectionContent").css('width', window_width-270+'px');
+            viewSection();
 
             self.sectionDisplay = function(section){
               self.sectionDisplayed = section;
@@ -24,7 +47,7 @@ angular.
 
             self.print = function(){
               self.sectionDisplayed = "print";
-              window.print();
+              $timeout(function(){alert("Il se peut que les couleurs de background et de font ne passent pas. Pour cela il faut préciser dans les options d'impiression qu il faille les couleurs de background.");window.print(); self.sectionDisplayed = "proExp";}, 100);
             }
         });
       }
